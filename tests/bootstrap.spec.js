356c9e54d30d9ee03904c3b30414c1f77c62687f
jnarlyv <jeanarly@gmail.com>
2023-03-19 19:08:51 -0400

    update boostrap test

--- tests/bootstrap.spec.js
+++ tests/bootstrap.spec.js
@@ -2,8 +2,10 @@ const { test, expect } = require('@playwright/test')
 
 test('Check for Bootstrap', async ({ page }) => {
   await page.goto('http://localhost:3000')
+
   const bootstrapPresent = await page.evaluate(() => {
-    return Boolean(document.querySelector('link[href*="bootstrap.min.css"]'))
+    return Boolean(window.bootstrap)
   })
+
   expect(bootstrapPresent).toBe(true)
 })
