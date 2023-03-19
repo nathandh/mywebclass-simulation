68c1cf65a73e0bfa7b974022a98076db8d66f83f
jnarlyv <jeanarly@gmail.com>
2023-03-19 16:05:29 -0400

    added privacy policy playwright test

--- tests/privacy_policy.spec.js
+++ tests/privacy_policy.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test')
 
 test('Check Privacy Policy Page', async ({ page }) => {
-  await page.goto('https://nathandh.github.io/mywebclass-simulation/privacy.html')
+  await page.goto('http://localhost:3000/privacy.html')
   const pageTitle = await page.title()
   expect(pageTitle).toBe('MyWebClass.org | Privacy Policy')
 })
