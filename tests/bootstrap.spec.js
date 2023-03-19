5abc24f037dd249801ea37db338b0691869a554c
jnarlyv <jeanarly@gmail.com>
2023-03-19 19:20:32 -0400

    update boostrap test again!

--- tests/bootstrap.spec.js
+++ tests/bootstrap.spec.js
@@ -1,11 +1,17 @@
-const { test, expect } = require('@playwright/test')
+const { test, expect } = require('@playwright/test');
 
-test('Check for Bootstrap', async ({ page }) => {
-  await page.goto('http://localhost:3000')
+test('Check if Bootstrap is loaded', async ({ page }) => {
+  await page.goto('http://localhost:3000');
 
-  const bootstrapPresent = await page.evaluate(() => {
-    return Boolean(window.bootstrap)
-  })
+  const isBootstrapLoaded = await page.evaluate(() => {
+    const links = document.getElementsByTagName('link');
+    for (const link of links) {
+      if (link.rel === 'stylesheet' && link.href.includes('bootstrap')) {
+        return true;
+      }
+    }
+    return false;
+  });
 
-  expect(bootstrapPresent).toBe(true)
-})
+  expect(isBootstrapLoaded).toBe(true);
+});
